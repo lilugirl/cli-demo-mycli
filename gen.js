3fd09f6bc95d4ee8696194f475867828312d6047
yi7.liu <yi7.liu@symbio.com>
2023-04-08 20:48:29 +0800

    init

---
+++ gen.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+var argv=process.argv;
+var filePath=__dirname;
+var currentPath=process.cwd();
+
+console.log('argv',argv)
+console.log('filePath',filePath)
+console.log('currentPath',currentPath)
